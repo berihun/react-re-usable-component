a76e644aad900c3db0edb05d7cc53706a3f72785
Berihun Hadis <berihun.haddis@rapp.com>
2023-08-21 12:19:33 +0300

    update done

---
+++ src/List.jsx
@@ -0,0 +1,21 @@
+const numbers = [1, 2, 3, 4];
+const List = (props) => (
+  <ul>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
+const Item = (props) => (
+  <li key={props.item.objectID}>
+    {props.item.objectID}
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+);
+
+export default List;
